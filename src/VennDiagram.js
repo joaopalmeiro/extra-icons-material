d67ba15e9e59b24f77cda019a84bfbb4ee58538b
João Palmeiro <joaommpalmeiro@gmail.com>
2021-12-28 22:59:25 +0000

    Update filename

---
+++ src/VennDiagram.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
+
+const SvgVennDiagram = (props) => (
+    <SvgIcon {...props}>
+        <path d="M12 18.326a7 7 0 1 1 0-12.653 7 7 0 1 1 0 12.653ZM10 16.9A6.977 6.977 0 0 1 8 12a6.98 6.98 0 0 1 2-4.9 5 5 0 1 0 0 9.8Zm4-9.8a6.977 6.977 0 0 1 2 4.9 6.977 6.977 0 0 1-2 4.9 5 5 0 1 0 0-9.8ZM12 8a4.993 4.993 0 0 0-2 4c0 1.636.786 3.088 2 4a4.993 4.993 0 0 0 2-4 4.993 4.993 0 0 0-2-4Z" />
+    </SvgIcon>
+);
+
+export default SvgVennDiagram;
